3dcc25e96489bec18a03116102973f65254479f1
zhangzhaohuan <zhangzhaohuan@ziggurat.cn>
2018-09-04 13:52:20 +0800

    [modify]使用nprogress插件

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import NProgress from 'nprogress';
+
+ class App extends Component {
+  componentWillMount(){
+    
+  }
+  render() {
+    return (
+      <div>
+       aaa
+      </div>
+    )
+  }
+}
+export default App
